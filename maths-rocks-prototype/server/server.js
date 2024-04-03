72a7482661866f7a511687259d32bf3b018d4970
MrOppenheimer <140056975+MrOppenheimer@users.noreply.github.com>
2024-04-03 18:45:12 +1300

    Delete maths-rocks-prototype directory

--- maths-rocks-prototype/server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const db_client = require("./db_client");
-const mathsRouter = require("./routes/maths");
-
-
-db_client;
-const app = express();
-app.use("/maths", mathsRouter);
-app.listen(5000, () => {console.log("Server started on port 5000.")});
