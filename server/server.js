dcf57eb6ae1a5d53feb52a790adb1c7b8b95a42f
Kelly <140401663+kwil492@users.noreply.github.com>
2024-04-19 04:06:06 +1200

    Update server.js to use profile route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const topicsRouter = require("./routes/topics");
+const profileRouter = require("./routes/profile");
 const Repository = require("./repository/repository");
 
 
@@ -8,6 +9,7 @@ async function start_server(){
     await Repository.getRepoInstance(); // Sync the database.
     const app = express();
     app.use("/topics", topicsRouter);
+    app.use("/profile", profileRouter);
     app.listen(5000, () => {console.log("Server started on port 5000.")});
 }
 
