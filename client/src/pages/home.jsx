7a276354ca1bbe7658dadef71e59384db3df5647
HelenEmmett <helen.emmett@gmail.com>
2024-04-18 19:25:45 +1200

    Added styles for the home page
    Updated the styles and added in the image.

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -9,15 +9,16 @@ export default function HomePage() {
       <Header />
         <main>
         <section className='section1 side-padding'>
-          <img src={mathsRocksLogo} alt="Maths Rocks logo" title="Maths Rocks" />
+          <img src={mathsRocksLogo} alt="Maths Rocks logo" title="Maths Rocks" className='main-logo' />
           <button className='floating-btn1'>Rocks</button>
           <button className='floating-btn2'>About</button>
           <button className='floating-btn3'>Leaderboard</button>
           </section>
           <section className='section2 side-padding'>
+            <span className='top-curve'></span>
             <div className='txt-overlay'>
               <p>What is maths rocks introduction paragraph ed eatur repeliqui vele ctorro ga. Num qui oditatio demquia quo earchite volo.</p>
-              <button>LEARN MORE</button>
+              <button className='btn'>LEARN MORE</button>
             </div>
           </section>
           <section className='section3'>
