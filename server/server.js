8be28a22a3cc953ae8ae6735d126a0e1d4ce2c03
Kelly <140401663+kwil492@users.noreply.github.com>
2024-04-19 03:20:58 +1200

    Update server.js to use leaderboard route

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const topicsRouter = require("./routes/topics");
+const leaderboardRouter = require("./routes/leaderboard");
 const Repository = require("./repository/repository");
-
-
 
 async function start_server(){
     await Repository.getRepoInstance(); // Sync the database.
     const app = express();
+    app.use(cors());
     app.use("/topics", topicsRouter);
-    app.listen(5000, () => {console.log("Server started on port 5000.")});
+    app.use("/leaderboard", leaderboardRouter);
+    app.listen(5000, () => {
+        console.log("Server started on port 5000.")
+    });
 }
-
 
 start_server();
