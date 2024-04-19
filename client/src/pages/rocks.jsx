405c702fe086329d9071cefdc70c2494f57e8649
alyssals24 <alyssalsnz@gmail.com>
2024-04-19 22:10:40 +1200

    created basic layout of the page

--- client/src/pages/rocks.jsx
+++ client/src/pages/rocks.jsx
@@ -6,7 +6,29 @@ export default function RocksPage() {
     <>
       <Header />
         <main>
-          <h1>List all rocks/topics</h1>
+          <h1>Rocks</h1>
+          <p>Browse rocks, or search for your favourite maths concept.</p>
+          <div className="rocks">
+            <form>
+              <select name="sortBy" id="sortBy">
+                <option default value>Sort by:</option>
+                <option value="rarity">Rarity of rock</option>
+                <option value="mostFound">Most found</option>
+                <option value="alphabet">Alphabetical</option>
+                <option value="number">Numerical</option>
+                </select>
+            </form>
+            <input type="text" size="20" placeholder="Search ..."></input>
+            <section id="rocksList">
+              <div>
+                <p>rock0</p>
+              </div>
+              <div>
+                <p>rock1</p>
+              </div>
+
+            </section>
+          </div>
         </main>
         <Footer />
     </>
