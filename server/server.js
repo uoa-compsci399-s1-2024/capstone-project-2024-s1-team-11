5b171a0dc1f3a49e6cd6e889f89446c1ea8a6a99
Kelly <140401663+kwil492@users.noreply.github.com>
2024-04-20 21:53:21 +1200

    Remove CORS

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const topicsRouter = require("./routes/topics");
 const leaderboardRouter = require("./routes/leaderboard");
 const Repository = require("./repository/repository");
@@ -7,7 +6,6 @@ const Repository = require("./repository/repository");
 async function start_server(){
     await Repository.getRepoInstance(); // Sync the database.
     const app = express();
-    app.use(cors());
     app.use("/topics", topicsRouter);
     app.use("/leaderboard", leaderboardRouter);
     app.listen(5000, () => {
