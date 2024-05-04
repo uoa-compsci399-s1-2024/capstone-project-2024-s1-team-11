a90eba05cad7844804e72329cdec2e760cfb2a5a
alyssals24 <alyssalsnz@gmail.com>
2024-05-04 22:30:07 +1200

    Added alphabetical and numerical sorting

--- client/src/pages/rocks.jsx
+++ client/src/pages/rocks.jsx
@@ -1,33 +1,37 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
 import React, {useState, useEffect} from 'react';
+//page is currently only fully functional with placeholder data
 
 const RocksPage = () => {
   const [topics, setTopics] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => { //doesn't work yet as the fetched data is arriving as objects, not an array so map function does not work
     const fetchTopics = async () => {
         const fetchPromise = await fetch('http://localhost:5000/topics');
-        const streamPromise = await response.json();
+        const streamPromise = await fetchPromise.json();
+        const data = Object.entries(streamPromise); //temporary fix to prevent errors
         setTopics(data);
     };
-    //fetchTopics();
+    fetchTopics();
 
   }, []);
 
   
-
+  //placeholder data
   useEffect(() => {
-    if(topics.length == 0) {
+    if(topics.length == 0) { //change the 0 to however many topic objects in the database to use placeholder data
       const randomTopics = [
-        {title: "zero", imageUri: "../placeholder.jpg", topic_id: 0}, {title: "one", imageUri: "../default_avatar.jpg", topic_id: 1},
-        {title: "two", imageUri: "none", topic_id: 2}
+        {title: "zero", imageUri: "../placeholder.jpg", topic_id: 0}, {title: "one", imageUri: "../placeholder.jpg", topic_id: 1},
+        {title: "two", imageUri: "../placeholder.jpg", topic_id: 2}
       ]
      setTopics(randomTopics);
     }
   }, [topics]);
 
-  console.log(topics);
+  //console.log(topics);
+
+  //search bar function
   const [query, setQuery] = useState("");
   const search_params = Object.keys(Object.assign({}, ...topics));
 
@@ -38,6 +42,29 @@ const RocksPage = () => {
       )
     );
   }
+
+  //dropdown sort function - can sort alphabetically and numerically
+  const [sortType, setSortType] = useState('numerical');
+
+  useEffect(() => {
+    const sortArray = type => {
+      const types = {
+        rarity: 'rarity',
+        mostFound: 'found',
+        alphabet: 'title',
+        number: 'topic_id',
+      };
+      const sortProperty = types[type];
+      let sorted = '';
+      if (sortProperty == 'topic_id') {
+        sorted = [...topics].sort((a, b) => a[sortProperty] - b[sortProperty]);
+      } else {
+        sorted = [...topics].sort((a, b) => a.title.localeCompare(b.title));
+      }
+      setTopics(sorted);
+    };
+    sortArray(sortType);
+  }, [sortType]);
   
 
 
@@ -51,7 +78,7 @@ const RocksPage = () => {
           </div>
           <div className="rocks">
             <form className="dropDownMenu">
-              <select name="sortBy" id="sortBy">
+              <select name="sortBy" id="sortBy" onChange={(e) => setSortType(e.target.value)}>
                 <option default value>Sort by:</option>
                 <option value="rarity">Rarity of rock</option>
                 <option value="mostFound">Most found</option>
