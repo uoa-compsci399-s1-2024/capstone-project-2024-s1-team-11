36a41b1461dfb7404882af7fbd25dc4af89c7db7
HelenEmmett <helen.emmett@gmail.com>
2024-05-04 16:50:15 +1200

    Added the rocks and user report static pages

--- client/src/pages/cms.jsx
+++ client/src/pages/cms.jsx
@@ -23,10 +23,14 @@ export default function CmsPage() {
            <h1>Content management system</h1>
            <div className='cms-nav'>
             <ul>
-                <li onClick={() => setDisplay({pages: true, topics: false, rocks: false, users: false})}>Pages</li>
-                <li onClick={() => setDisplay({pages: false, topics: true, rocks: false, users: false})}>Topic pages</li>
-                <li onClick={() => setDisplay({pages: false, topics: false, rocks: true, users: false})}>Rocks</li>
-                <li onClick={() => setDisplay({pages: false, topics: false, rocks: false, users: true})}>Users</li>
+                <li onClick={() => setDisplay({pages: true, topics: false, rocks: false, users: false})}
+                 className={display.pages ? 'active' : ''}>Pages</li>
+                <li onClick={() => setDisplay({pages: false, topics: true, rocks: false, users: false})}
+                className={display.topics ? 'active' : ''}>Topic pages</li>
+                <li onClick={() => setDisplay({pages: false, topics: false, rocks: true, users: false})}
+                className={display.rocks ? 'active' : ''}>Rocks</li>
+                <li onClick={() => setDisplay({pages: false, topics: false, rocks: false, users: true})}
+                className={display.users ? 'active' : ''}>Users</li>
             </ul>
            </div>
            {display.pages ? <CmsPages /> : null}
