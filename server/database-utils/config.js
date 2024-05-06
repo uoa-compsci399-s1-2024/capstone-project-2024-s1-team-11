44f169967361592709a37adaa9b2d0d641fc740f
aq-parzival <80031725+aq-parzival@users.noreply.github.com>
2024-05-06 18:08:25 +1200

    nothing

--- server/database-utils/config.js
+++ server/database-utils/config.js
@@ -1,7 +1,7 @@
 const config = {
     db_name: 'maths_rocks',
     username: 'postgres',
-    password: 'Bubblegum11',
+    password: 'passwordhere',
     host: 'localhost',
     port: 5432,
     dialect: 'postgres'
