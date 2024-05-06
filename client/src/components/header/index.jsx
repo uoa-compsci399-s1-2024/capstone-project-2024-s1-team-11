88407239ffe751a1f01936b36d93139ff05823e1
HelenEmmett <helen.emmett@gmail.com>
2024-05-06 16:17:01 +1200

    Updated navigation
    Removed hardcoded topic from navigation.

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -8,19 +8,17 @@ import Cookies from "js-cookie";
 
 function Navigation() {
   return (
+    <>
     <ul>
         <li><NavLink to={`/`}>Home</NavLink></li>
         <li><NavLink to={`/about`}>About Maths Rocks</NavLink></li>
-        <li><NavLink to={`/rocks`}>Rocks & Maths Topics</NavLink>
-          <ul>
-            <li><NavLink to={`/rocks/1`}>0</NavLink></li>
-          </ul>
-        </li>
+        <li><NavLink to={`/rocks`}>Maths Topics</NavLink></li>
         <li><NavLink to={`/leaderboard`}>Leaderboard</NavLink></li>
-        <li><NavLink to={`/profile`}>My Profile</NavLink></li>
         <li><NavLink to={`/contact`}>Contact Us</NavLink></li>
         <li><NavLink to={`/privacy-policy`}>Privacy Policy</NavLink></li>
+        <li><NavLink to={`/profile`}>My Profile</NavLink></li>
     </ul>
+    </>
   )
 }
 
