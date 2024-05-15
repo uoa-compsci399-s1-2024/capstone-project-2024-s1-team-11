c2d6035c76c70208f554bd50256e2b138839d514
Kelly <140401663+kwil492@users.noreply.github.com>
2024-05-15 22:34:18 +1200

    Update leaderboard.jsx

--- client/src/pages/leaderboard.jsx
+++ client/src/pages/leaderboard.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import '../leaderboard_styles.css';
-import API from '../../api.js'
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
@@ -11,7 +10,7 @@ const Leaderboard = () => {
   useEffect(() => {
     const fetchLeaderboardData = async () => {
       try {
-        let url = API + '/leaderboard';
+        let url = 'http://localhost:5000/leaderboard';
         if (monthly) {
           url += '/monthly';
         }
@@ -33,46 +32,49 @@ const Leaderboard = () => {
     <>
       <Header />
       <main>
-      <article className='side-padding top-padding'>
-        <h1 className="leaderboard-header">Leaderboard</h1>
-        <div className="toggle-buttons">
-          <button onClick={() => setMonthly(false)} className={!monthly ? "active" : ""}>All Time</button>
-          <button onClick={() => setMonthly(true)} className={monthly ? "active" : ""}>Monthly</button>
-        </div>
-        <table className="leaderboard-table">
-          <thead>
-            <tr>
-              <th className="left-align">
-                <span className="rank-heading">Rank</span>
-              </th>
-              <th className="left-align">
-                <span className="user-heading">User</span>
-              </th>
-              <th className="left-align">
-                <span className="rocks-heading">Rocks</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboardData.map((user, index) => (
-              <tr key={index}>
-                <td>
-                  {index > 0 && user.rock_count === leaderboardData[index - 1].rock_count
-                    ? `#${index}`
-                    : `#${index + 1}`}
-                </td>
-                <td className="user-cell">
-
-                  <span className="username">{user.username}</span>
-
-                    <img src="/default_avatar.jpg" alt="Avatar" className="avatar"/>
-
-                </td>
-                <td style={{ textAlign: 'right' }}>{user.rock_count}</td>
+        <article className='side-padding top-padding'>
+          <h1 className="leaderboard-header">Leaderboard</h1>
+          <div className="toggle-buttons">
+            <button onClick={() => setMonthly(false)} className={!monthly ? "active" : ""}>All Time</button>
+            <button onClick={() => setMonthly(true)} className={monthly ? "active" : ""}>Monthly</button>
+          </div>
+          <table className="leaderboard-table">
+            <thead>
+              <tr>
+                <th className="left-align">
+                  <span className="rank-heading">Rank</span>
+                </th>
+                <th className="left-align">
+                  <span className="user-heading">User</span>
+                </th>
+                <th className="left-align">
+                  <span className="rocks-heading">Rocks</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {leaderboardData.map((user, index) => (
+                <tr key={index}>
+                  <td>
+                    {index > 0 && user.rock_count === leaderboardData[index - 1].rock_count
+                      ? `#${index}`
+                      : `#${index + 1}`}
+                  </td>
+                  <td className="user-cell">
+                    <div className="avatar-wrapper">
+                      {user && user.avatar ? (
+                        <img src={`/${user.avatar}`} alt="Profile Picture" className="avatar" />
+                      ) : (
+                        <img src="/avatar-00.jpg" alt="Default Profile Picture" className="avatar" />
+                      )}
+                    </div>
+                    <span className="username">{user.username}</span>
+                  </td>
+                  <td style={{ textAlign: 'right' }}>{user.rock_count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </article>
       </main>
       <Footer />
